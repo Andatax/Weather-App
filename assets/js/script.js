43a3b2bfb10541a3f3e44030f486e91c0b25f84e
Andatax <140019816+Andatax@users.noreply.github.com>
2023-10-19 21:46:14 -0400

    Added function to store wather cards in the local data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,87 +1,87 @@
-const weatherAppApiKey = 'fe0b392100981c0ca23437f4c874b5e8';
-const citiesApiKey = 'UV6648NeZT4RY5GoyCFnDSijoyoNqcMMuF33K3fD';
+const weatherAppApiKey = "fe0b392100981c0ca23437f4c874b5e8";
+const citiesApiKey = "UV6648NeZT4RY5GoyCFnDSijoyoNqcMMuF33K3fD";
 
 // Define an object that maps weather icons to their corresponding CSS classes
 const weatherIcons = {
-	'04n': 'bi-clouds',
-	'04d': 'bi-clouds',
-	'09n': ' bi-cloud-drizzle',
-	'09d': 'bi-cloud-drizzle',
-	'10n': 'bi-cloud-rain-heavy-fill',
-	'10d': 'bi-cloud-rain-heavy-fill',
-	'01n': 'bi-brightness-high-fill',
-	'01d': 'bi-brightness-high-fill',
-	'02n': 'bi-cloud-moon-fill',
-	'02d': 'bi-cloud-sun-fill',
-	'03n': 'bi-cloud-fill',
-	'03d': 'bi-cloud-fill',
-	'11n': 'bi-cloud-lightning-fill',
-	'11d': 'bi-cloud-lightning-fill',
-	'13n': 'bi-snow',
-	'13d': 'bi-snow',
-	'50n': 'bi-cloud-fog',
-	'50d': 'bi-cloud-fog',
+	"04n": "bi-clouds",
+	"04d": "bi-clouds",
+	"09n": " bi-cloud-drizzle",
+	"09d": "bi-cloud-drizzle",
+	"10n": "bi-cloud-rain-heavy-fill",
+	"10d": "bi-cloud-rain-heavy-fill",
+	"01n": "bi-brightness-high-fill",
+	"01d": "bi-brightness-high-fill",
+	"02n": "bi-cloud-moon-fill",
+	"02d": "bi-cloud-sun-fill",
+	"03n": "bi-cloud-fill",
+	"03d": "bi-cloud-fill",
+	"11n": "bi-cloud-lightning-fill",
+	"11d": "bi-cloud-lightning-fill",
+	"13n": "bi-snow",
+	"13d": "bi-snow",
+	"50n": "bi-cloud-fog",
+	"50d": "bi-cloud-fog",
 };
 
 // Define an object that stores HTML selectors for various elements on the page
 // These selectors are used to update the HTML elements with the corresponding data
 const htmlSelectors = {
 	day1: [
-		$('#day1'),
-		$('#weather1'),
-		$('#weatherIcon1'),
-		$('#temp1'),
-		$('#minMaxTemp1'),
-		$('#wind1'),
-		$('#humidity1'),
+		$("#day1"),
+		$("#weather1"),
+		$("#weatherIcon1"),
+		$("#temp1"),
+		$("#minMaxTemp1"),
+		$("#wind1"),
+		$("#humidity1"),
 	],
 	day2: [
-		$('#day2'),
-		$('#weather2'),
-		$('#weatherIcon2'),
-		$('#temp2'),
-		$('#minMaxTemp2'),
-		$('#wind2'),
-		$('#humidity2'),
+		$("#day2"),
+		$("#weather2"),
+		$("#weatherIcon2"),
+		$("#temp2"),
+		$("#minMaxTemp2"),
+		$("#wind2"),
+		$("#humidity2"),
 	],
 	day3: [
-		$('#day3'),
-		$('#weather3'),
-		$('#weatherIcon3'),
-		$('#temp3'),
-		$('#minMaxTemp3'),
-		$('#wind3'),
-		$('#humidity3'),
+		$("#day3"),
+		$("#weather3"),
+		$("#weatherIcon3"),
+		$("#temp3"),
+		$("#minMaxTemp3"),
+		$("#wind3"),
+		$("#humidity3"),
 	],
 	day4: [
-		$('#day4'),
-		$('#weather4'),
-		$('#weatherIcon4'),
-		$('#temp4'),
-		$('#minMaxTemp4'),
-		$('#wind4'),
-		$('#humidity4'),
+		$("#day4"),
+		$("#weather4"),
+		$("#weatherIcon4"),
+		$("#temp4"),
+		$("#minMaxTemp4"),
+		$("#wind4"),
+		$("#humidity4"),
 	],
 	day5: [
-		$('#day5'),
-		$('#weather5'),
-		$('#weatherIcon5'),
-		$('#temp5'),
-		$('#minMaxTemp5'),
-		$('#wind5'),
-		$('#humidity5'),
+		$("#day5"),
+		$("#weather5"),
+		$("#weatherIcon5"),
+		$("#temp5"),
+		$("#minMaxTemp5"),
+		$("#wind5"),
+		$("#humidity5"),
 	],
 	dayMain: [
-		$('#mainDay'),
-		$('#weatherMain'),
-		$('#weatherIconMain'),
-		$('#tempMain'),
-		$('#minMaxTempMain'),
-		$('#windMain'),
-		$('#humidityMain'),
+		$("#mainDay"),
+		$("#weatherMain"),
+		$("#weatherIconMain"),
+		$("#tempMain"),
+		$("#minMaxTempMain"),
+		$("#windMain"),
+		$("#humidityMain"),
 	],
-	citySearch: $('#searchInput'),
-	searchButton: $('#searchBtn'),
+	citySearch: $("#searchInput"),
+	searchButton: $("#searchBtn"),
 };
 
 if (
@@ -93,11 +93,11 @@ if (
 
 		// Make a request to the cities API to get the latitude and longitude of the input city
 
-		fetch('https://api.api-ninjas.com/v1/city?name=' + input, {
-			method: 'GET',
+		fetch("https://api.api-ninjas.com/v1/city?name=" + input, {
+			method: "GET",
 			headers: {
-				'X-Api-Key': citiesApiKey,
-				'Content-Type': 'application/json',
+				"X-Api-Key": citiesApiKey,
+				"Content-Type": "application/json",
 			},
 		})
 			.then((response) => {
@@ -106,7 +106,7 @@ if (
 			.then((result) => {
 				// console.log(result);
 				if (result.length === 0) {
-					alert('City not found');
+					alert("City not found");
 				}
 				// Extract the latitude and longitude from the API response
 
@@ -143,18 +143,18 @@ if (
 							}
 						}
 						htmlSelectors.dayMain[3].text(
-							'TEMPERATURE: ' + Math.ceil(currentWeatherData.main.temp - 273.15) + ' °C'
+							"TEMPERATURE: " + Math.ceil(currentWeatherData.main.temp - 273.15) + " °C"
 						);
 						htmlSelectors.dayMain[4].text(
-							'MAX/MIN: ' +
+							"MAX/MIN: " +
 								Math.floor(currentWeatherData.main.temp_max - 273.15) +
-								' °C ' +
+								" °C " +
 								Math.ceil(currentWeatherData.main.temp_min - 273.15) +
-								' °C'
+								" °C"
 						);
-						htmlSelectors.dayMain[5].text('WIND: ' + Math.ceil(currentWeatherData.wind.speed) + ' m/s');
+						htmlSelectors.dayMain[5].text("WIND: " + Math.ceil(currentWeatherData.wind.speed) + " m/s");
 						htmlSelectors.dayMain[6].text(
-							'HUMIDITY: ' + Math.ceil(currentWeatherData.main.humidity) + ' %'
+							"HUMIDITY: " + Math.ceil(currentWeatherData.main.humidity) + " %"
 						);
 					})
 					.catch((error) => {
@@ -163,51 +163,68 @@ if (
 			})
 			.then()
 			.catch((error) => {
-				console.error('Error: ', error);
+				console.error("Error: ", error);
 			});
 	})
 );
 
 const updateWeatherCard = (weatherData, listIndex, dayIndex) => {
-	console.log(weatherData);
-	let date = weatherData.list[listIndex].dt;
-	htmlSelectors[`day${dayIndex}`][0].text(`${timeConverter(date)}`);
-	htmlSelectors[`day${dayIndex}`][1].text(
-		weatherData.list[listIndex].weather[0].description.toUpperCase()
-	);
+	// Other code...
+
+	const temperature = Math.ceil(weatherData.list[listIndex].main.temp - 273.15);
+	const maxTemperature = Math.floor(weatherData.list[listIndex - 2].main.temp_max - 273.15);
+	const minTemperature = Math.ceil(weatherData.list[listIndex + 2].main.temp_min - 273.15);
+	const windSpeed = Math.ceil(weatherData.list[listIndex].wind.speed);
+	const humidity = Math.ceil(weatherData.list[listIndex].main.humidity);
+	const description = weatherData.list[listIndex].weather[0].description.toUpperCase();
+	const date = timeConverter(weatherData.list[listIndex].dt);
+	let icon = "";
+
 	for (const key in weatherIcons) {
 		if (key === weatherData.list[listIndex].weather[0].icon) {
-			// console.log(key);
-			let icon = weatherIcons[key];
+			icon = weatherIcons[key];
 			htmlSelectors[`day${dayIndex}`][2].addClass(icon.toString());
+			break;
 		}
 	}
-	htmlSelectors[`day${dayIndex}`][3].text(
-		'TEMPERATURE: ' + Math.ceil(weatherData.list[listIndex].main.temp - 273.15) + ' °C'
-	);
+
+	const cardData = {
+		temperature,
+		maxTemperature,
+		minTemperature,
+		windSpeed,
+		humidity,
+		description,
+		date,
+		icon,
+	};
+	saveCards(cardData);
+	htmlSelectors[`day${dayIndex}`][0].text(date);
+	htmlSelectors[`day${dayIndex}`][1].text(description);
+	htmlSelectors[`day${dayIndex}`][2].addClass(icon.toString());
+	htmlSelectors[`day${dayIndex}`][3].text("TEMPERATURE: " + temperature + " °C");
 	htmlSelectors[`day${dayIndex}`][4].text(
-		'MAX/MIN: ' +
-			Math.floor(weatherData.list[listIndex - 2].main.temp_max - 273.15) +
-			' °C ' +
-			Math.ceil(weatherData.list[listIndex + 2].main.temp_min - 273.15) +
-			' °C'
+		"MAX/MIN: " + maxTemperature + " °C " + minTemperature + " °C"
 	);
-	htmlSelectors[`day${dayIndex}`][5].text(
-		'WIND: ' + Math.ceil(weatherData.list[listIndex].wind.speed) + ' m/s'
-	);
-	htmlSelectors[`day${dayIndex}`][6].text(
-		'HUMIDITY: ' + Math.ceil(weatherData.list[listIndex].main.humidity) + ' %'
-	);
+	htmlSelectors[`day${dayIndex}`][5].text("WIND: " + windSpeed + " m/s");
+	htmlSelectors[`day${dayIndex}`][6].text("HUMIDITY: " + humidity + " %");
+	return cardData;
+};
+
+let saveCards = (cardData) => {
+	let savedCards = JSON.parse(window.localStorage.getItem("savedCards")) || [];
+	savedCards.push(cardData);
+	window.localStorage.setItem("savedCards", JSON.stringify(savedCards));
 };
 
 // Converts a UNIX timestamp to a formatted date string
 // Main part of the function timeConverter was taken from stack overflow and it was adapted to my code and my logic
 function timeConverter(UNIX_timestamp) {
-	var a = new Date(UNIX_timestamp * 1000);
-	var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-	var year = a.getFullYear();
-	var month = months[a.getMonth()];
-	var day = a.getDate();
-	var date = day + ' ' + month + ', ' + year;
+	let a = new Date(UNIX_timestamp * 1000);
+	let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+	let year = a.getFullYear();
+	let month = months[a.getMonth()];
+	let day = a.getDate();
+	let date = day + " " + month + ", " + year;
 	return date;
 }
